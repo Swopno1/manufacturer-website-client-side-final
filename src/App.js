ab27da81c2b6578995f0af00d5805aa18489b029
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-05-25 10:42:41 +0600

    Added ne route My Portfolio.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./Pages/Home/Home";
-import Login from "./Pages/Login/Login";
-import Register from "./Pages/Login/Register";
-import RequireAuth from "./Pages/Login/RequireAuth";
-import Purchase from "./Pages/Purchase/Purchase";
-import Footer from "./Shared/Footer";
-import Nav from "./Shared/Nav";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Login/Register';
+import RequireAuth from './Pages/Login/RequireAuth';
+import Portfolio from './Pages/Portfolio/Portfolio';
+import Purchase from './Pages/Purchase/Purchase';
+import Footer from './Shared/Footer';
+import Nav from './Shared/Nav';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Nav></Nav>
       <Routes>
-        <Route path="/" element={<Home></Home>} />
+        <Route path='/' element={<Home></Home>} />
         <Route
-          path="/purchase"
+          path='/purchase'
           element={
             <RequireAuth>
               <Purchase></Purchase>
             </RequireAuth>
           }
         />
-        <Route path="/login" element={<Login></Login>} />
-        <Route path="/register" element={<Register></Register>} />
+        <Route
+          path='/tools/:id'
+          element={
+            <RequireAuth>
+              <Purchase></Purchase>
+            </RequireAuth>
+          }
+        />
+        <Route path='/portfolio' element={<Portfolio></Portfolio>} />
+        <Route path='/login' element={<Login></Login>} />
+        <Route path='/register' element={<Register></Register>} />
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Home/ToolCard.js
+++ src/Pages/Home/ToolCard.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ToolCard = ({ tool }) => {
-  const { name, img, description, minOrderQty, availableQty } = tool;
+  const navigate = useNavigate();
+
+  const { _id, name, img, description, minOrderQty, availableQty } = tool;
   return (
-    <div className="card w-96">
+    <div className='card w-96'>
       <figure>
         <img src={img} alt={name} />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">{name}</h2>
+      <div className='card-body'>
+        <h2 className='card-title'>{name}</h2>
         <p>{description}</p>
-        <div className="card-actions justify-between">
+        <div className='card-actions justify-between'>
           <p>{`Min Order: ${minOrderQty}`}</p>
           <p>{`Available Stock: ${availableQty}`}</p>
           <p>{`Price: ${availableQty}`}</p>
-          <button className="btn btn-primary">Buy Now</button>
+          <button
+            className='btn btn-primary'
+            onClick={() => navigate(`/tools/${_id}`)}
+          >
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
